9d0f69e4c99a9eff13d61cf86b705cf407edcd87
serhiizem <serhiizem@gmail.com>
2018-05-24 21:35:55 +0300

    Initial commit

---
+++ auth-server/src/main/java/telecom/authserver/AuthConfig.java
@@ -0,0 +1,57 @@
+package telecom.authserver;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+import org.springframework.web.servlet.view.RedirectView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+@Configuration
+@EnableAuthorizationServer
+public class AuthConfig extends AuthorizationServerConfigurerAdapter {
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Override
+    public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+        clients.inMemory().withClient("acme").secret("acmesecret")
+                .authorizedGrantTypes("authorization_code", "refresh_token",
+                        "password")
+                .autoApprove(true)
+                .scopes("openid");
+    }
+
+    @Override
+    public void configure(AuthorizationServerEndpointsConfigurer endpoints) {
+        endpoints.authenticationManager(authenticationManager);
+
+        endpoints.addInterceptor(new HandlerInterceptorAdapter() {
+            @Override
+            public void postHandle(HttpServletRequest request,
+                                   HttpServletResponse response, Object handler,
+                                   ModelAndView modelAndView) throws Exception {
+                if (modelAndView != null
+                        && modelAndView.getView() instanceof RedirectView) {
+                    RedirectView redirect = (RedirectView) modelAndView.getView();
+                    String url = redirect.getUrl();
+                    if (url.contains("code=") || url.contains("error=")) {
+                        HttpSession session = request.getSession(false);
+                        if (session != null) {
+                            session.invalidate();
+                        }
+                    }
+                }
+            }
+        });
+    }
+}
