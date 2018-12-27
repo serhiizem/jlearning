19942d303650ffe091ea9144446d68b4c9b46769
serhiizem <serhiizem@gmail.com>
2018-12-27 15:48:55 +0200

    Migration to Spring boot 2, Junit 5

--- tariffs-component/src/test/java/telecom/dao/TariffDaoTest.java
+++ tariffs-component/src/test/java/telecom/dao/TariffDaoTest.java
@@ -1,10 +1,12 @@
 package telecom.dao;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
 
-
+@ExtendWith(SpringExtension.class)
+@SpringJUnitConfig()
 public class TariffDaoTest {
 
     @Test
